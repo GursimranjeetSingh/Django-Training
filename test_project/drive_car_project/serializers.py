527009cd25ace8084fec846a8123502473db3417
GursimranjeetSingh <gursimranjeets@2be.ae>
2023-03-30 13:28:55 +0530

    new code

--- test_project/drive_car_project/serializers.py
+++ test_project/drive_car_project/serializers.py
@@ -13,6 +13,26 @@ class CarSerializer(serializers.ModelSerializer):
         
         
 class DriverSerializer(serializers.ModelSerializer):
+    name=serializers.SerializerMethodField()
+    small_name=serializers.SerializerMethodField()
+    # car=serializers.CharField(source='car.name') #gives single field
+    car=CarSerializer()  #gives entire data
+    simple=serializers.CharField(default='simple')
+    
+    
+    def get_name(self,obj):
+        name=obj.name
+        name=name.upper()
+        return name
+    
+    def get_small_name(self,obj):
+        name=obj.name
+        name=name.lower()
+        return name
+    
     class Meta:
         model=Driver
         fields='__all__'
+        # fields=['name','age','car']
+        extra_fields=['small_name','simple']
+        # exclude=['age']
