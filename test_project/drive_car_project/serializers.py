3eae34b29540e87d7d0f3b76a4ecdb9811fa4883
GursimranjeetSingh <gursimranjeets@2be.ae>
2023-04-04 16:16:45 +0530

    new code with pagination added

--- test_project/drive_car_project/serializers.py
+++ test_project/drive_car_project/serializers.py
@@ -2,6 +2,7 @@ from .models import Car,Driver
 
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
 
 
 class CarSerializer(serializers.ModelSerializer):
@@ -9,6 +10,13 @@ class CarSerializer(serializers.ModelSerializer):
         model=Car
         fields='__all__'
         
+        
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields='__all__'
         
         
         
--- test_project/drive_car_project/views.py
+++ test_project/drive_car_project/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from .models import *
 from .serializers import *
 from rest_framework.viewsets import ModelViewSet,ViewSet
@@ -10,10 +9,65 @@ from rest_framework import filters
 from django.contrib.auth.models import User
 
 from rest_framework_simplejwt.tokens import AccessToken
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
+
+
+
+class SignUpViewSet(ViewSet):
+    
+    def create(self,request):
+        try:
+            
+            username=request.data.get('username')
+            password=request.data.get('password')
+            
+            
+            data={
+                'username':username,
+                'password':password,
+            }
+            
+            # user=UserSerializer(data=data)
+            # user.is_valid(raise_exception=True)
+            
+            user=User.objects.create_user(**data)
+            # user=User.objects.create_user(username=username,password=password)
+            # user=User.objects.create(username=username,password=password)
+            
+            # if user.is_valid():
+            #     user.save()
+            user.save()
+            
+            data={
+                'status':True,
+                'message':'User created successfully'
+            }
+            return Response(data)
+        except Exception as e:
+            data={
+                'status':False,
+                'message':'Failed to create user',
+                'error':str(e)
+            }
+            return Response(data)
+        
+        
+    def list(self,request):
+        user=User.objects.all()
+        serializers=UserSerializer(user,many=True)
+        data=serializers.data
+        data={
+            'data':data,
+            'status':True,
+        }
+        return Response(data)
+            
+        
+        
+    
 
 
 
@@ -56,12 +110,8 @@ class LoginViewSet(ViewSet):
 class CarViewSet(ModelViewSet):
     queryset=Car.objects.all()
     serializer_class=CarSerializer
-    permission_classes=[IsAuthenticated]
     filter_backends = [filters.SearchFilter]
     search_fields=['name','color']
-    
-    
-    
     
     def list(self, request, *args, **kwargs):
         # self.get_queryset()
@@ -69,16 +119,14 @@ class CarViewSet(ModelViewSet):
         # self.filter_queryset()
         # self.paginte_queryset()
         
-        # querset=Car.objects.all()  # 
+        queryset=Car.objects.all()  # 
         
         user=request.user #get user from request
         user=user.id
-        
-        
-        querset=self.get_queryset().filter(created_by=user.id)
+        # queryset=self.get_queryset()
+        queryset=self.paginate_queryset(queryset)
 
-
-        queryset=self.filter_queryset(querset)
+        queryset=self.filter_queryset(queryset)
         
         # serializers=CarSerializer(queryset,many=True)
         serializers=self.get_serializer(queryset,many=True)
